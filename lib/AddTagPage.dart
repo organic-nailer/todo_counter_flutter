090a5f1c4f8bd0680e67c70a71a1a20bd75529a0
速川湧気 <hykwyuk0125@gmail.com>
2019-02-24 20:08:03 +0900

    タグの検索機能を削除

--- lib/AddTagPage.dart
+++ lib/AddTagPage.dart
@@ -10,9 +10,6 @@ class AddTagPageState extends State<AddTagPage>{
 
 	var _selected = new List<String>();
 
-	List<String> _searchResult = [];
-
-	final TextEditingController _SearchTextController = new TextEditingController();
 	final TextEditingController _AddTagDialogController = new TextEditingController();
 
 	@override
@@ -21,7 +18,7 @@ class AddTagPageState extends State<AddTagPage>{
     return new Scaffold(
 	    //検索ボックス
 	    appBar: new AppBar(
-		    title: new Text("Tag"),
+		    title: new Text("タグ"),
 		    actions: <Widget>[
 		    	new IconButton(
 				    icon: new Icon(
@@ -35,47 +32,8 @@ class AddTagPageState extends State<AddTagPage>{
 				    	Navigator.of(context).pop(_selected.join(","));
 				    }),
 		    ],
-		    bottom: new PreferredSize(
-			    preferredSize:  const Size.fromHeight(48.0),
-		      child: Padding(
-		        padding: const EdgeInsets.all(0.0),
-		        child: new ListTile(
-					leading: new Icon(Icons.search, color: Colors.white,),
-					title: new TextField(
-						controller: _SearchTextController,
-						style: new TextStyle(
-							color: Colors.white,
-						),
-						decoration: new InputDecoration(
-					        hintText: 'タグを検索...',
-							border: InputBorder.none,
-							hintStyle: new TextStyle(
-								color: Colors.white,
-							)
-						),
-				        onChanged: onSearchTextChanged,),
-			        trailing: new IconButton(
-				        icon: new Icon(
-					        Icons.cancel,
-					        color: Colors.white,
-				        ),
-				        onPressed: () {
-				        	_SearchTextController.clear();
-				        	onSearchTextChanged('');
-				        	},
-			        ),
-		        ),
-		      ),
-		    ),
 	    ),
-	    //検索ボックスが空でないもしくは検索結果がある場合は検索結果を表示、それ以外は全部を表示
-	    body: _searchResult.length != 0 || _SearchTextController.text.isNotEmpty
-	    ? new ListView.builder(
-		    itemCount: _searchResult.length,
-		    padding: const EdgeInsets.only(top: 10.0),
-		    itemBuilder: (context, index) => _TagItemViewByTxt(_searchResult[index]),
-	    )
-	    : new StreamBuilder(
+	    body: new StreamBuilder(
 		    stream: Firestore.instance.collection("Tags").snapshots(),
 		    builder: (context,snapshot){
 			    if(!snapshot.hasData) return const Text("Loading...");
@@ -89,75 +47,22 @@ class AddTagPageState extends State<AddTagPage>{
     );
 	}
 
-	onSearchTextChanged(String text) async {
-		_searchResult.clear();
-		if (text.isEmpty) {
-			setState(() {});
-			return;
-		}
-
-		Firestore.instance.collection("Tags").where("tag",isGreaterThanOrEqualTo: text).snapshots().listen(
-			(data) => _searchResult.add(data.documents[0]["tag"])
-		);
-
-		setState(() {});
-	}
-
-	Widget TagListToggleView(){
-		if(_searchResult.length != 0 || _SearchTextController.text.isNotEmpty)
-		{
-			return new ListView.builder(
-				itemCount: _searchResult.length,
-				padding: const EdgeInsets.only(top: 10.0),
-				itemBuilder: (context, index) => _TagItemViewByTxt(_searchResult[index]),
-			);
-		}
-		else {
-			return new StreamBuilder(
-				stream: Firestore.instance.collection("Tags").snapshots(),
-				builder: (context,snapshot){
-					if(!snapshot.hasData) return const Text("Loading...");
-
-					if(_searchResult.length == 0){
-						return new ListTile(
-							title: new Text(_SearchTextController.text + " を追加..."),
-							trailing: new CircleAvatar(
-								backgroundColor: Colors.green,
-								child: new IconButton(
-									icon: new Icon(Icons.add),
-									onPressed: (){
-
-									}),
-							),
-						);
-					}
-
-					return new ListView.builder(
-						itemCount: snapshot.data.documents.length,
-						padding: const EdgeInsets.only(top: 10.0),
-						itemBuilder: (context, index) => TagItemView(snapshot.data.documents[index]),
-					);
-				}
-			);
-		}
-	}
-
 	///タグを表示させるリストのアイテム
 	Widget TagItemView(DocumentSnapshot document){
 		return _TagItemViewByTxt(document["tag"]);
 	}
-	Widget _TagItemViewByTxt(String Tag){
-		bool IsSelect = _selected.contains(Tag);
+	Widget _TagItemViewByTxt(String _tag){
+		bool _isSelect = _selected.contains(_tag);
 
 		return new CheckboxListTile(
-			title: new Text(Tag),
-			value: IsSelect,
+			title: new Text(_tag),
+			value: _isSelect,
 			onChanged: (next){
-				if(next) _selected.add(Tag);
-				else _selected.remove(Tag);
+				if(next) _selected.add(_tag);
+				else _selected.remove(_tag);
 
 				setState(() {
-					IsSelect = next;
+					_isSelect = next;
 				});
 			}
 		);
